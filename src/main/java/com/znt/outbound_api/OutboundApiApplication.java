5337366e01b32cefe52a903520df50ae954e8b8d
jeannyChiu <jeannychiu608@gmail.com>
2025-06-06 15:14:06 +0800

    Initial commit

---
+++ src/main/java/com/znt/outbound_api/OutboundApiApplication.java
@@ -0,0 +1,13 @@
+package com.znt.outbound_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboundApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutboundApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/znt/outbound_api/OutboundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znt.outbound_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboundApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
